ddf136e5a86e275f2dd3ef9a7c864649493c47a1
Maxime B <mbmsc2025@gmail.com>
2023-09-21 15:52:20 -0400

    revisionf

--- q2_script.py
+++
@@ -1,6 +0,0 @@
-longueur = float(input("Entrez la longueur du rectangle: "))
-largeur = float(input("Entrez la largeur du rectangle: "))
-perimetre_du_rectangle = 2 * (longueur + largeur)
-aire_du_rectangle = longueur * largeur
-print(perimetre_du_rectangle)
-print(aire_du_rectangle)
